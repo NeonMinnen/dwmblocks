b37de2408626145aabd73673817c40388cc1fd66
AutoDys <matt@n3on.io>
2023-09-18 01:32:52 -0700

    Changed update intervals

--- blocks.def.h
+++ blocks.def.h
@@ -2,13 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"CPU:" , "~/.local/bin/cpu",                       5,      0} ,
-	{"GPU:" , "~/.local/bin/gpu",                       5,      0} ,
+	{"CPU:" , "~/.local/bin/cpu",                       10,      0} ,
+	{"GPU:" , "~/.local/bin/gpu",                       10,      0} ,
     {"" , "~/.local/bin/volume1",                       0,     10} ,
-	{"Home:" , "~/.local/bin/homespace",                5,      0} ,
-	{"Root:" , "~/.local/bin/rootspace",                5,      0} ,
+	{"Home:" , "~/.local/bin/homespace",                180,     0} ,
+	{"Root:" , "~/.local/bin/rootspace",                180,     0} ,
 	{"Updates:" , "~/.local/bin/pacman",                300,    0} ,
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'",					60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
